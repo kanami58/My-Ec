149346561935894fc75f7d2cd566b3bf7e17ca32
5bit-goya <k.goya@5-bit.jp>
2023-09-15 19:37:19 +0900

    フロントエンドディレクトリの初期化

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { Box, Button } from "@mui/material"
+
+function App() {
+
+  return (
+    <>
+      <Box display={"flex"} width={"100%"} bgcolor={"green"}>
+        <Button>
+          あああ
+        </Button>
+      </Box>
+    </>
+  )
+}
+
+export default App
